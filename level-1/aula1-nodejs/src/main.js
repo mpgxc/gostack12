9e782c33f7e064fe50377feee12ffe2455c0bdbd
mpgxc <mpgx5.c@gmail.com>
2020-06-16 06:11:33 -0300

    feat(nodejs): add cors

--- level-1/aula1-nodejs/src/main.js
+++ level-1/aula1-nodejs/src/main.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { uuid, isUuid } = require('uuidv4');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Mid Local
 const validateProjectId = (request, response, next) => {
