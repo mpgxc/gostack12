b27988261a38da3479d1e486e9ea4728647826ed
mpgxc <mpgx5.c@gmail.com>
2020-06-18 04:56:14 -0300

    feat(react-native): aula rn done 🎉

--- level-1/aula3-react-native/index.js
+++ level-1/aula3-react-native/index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from './src/index';
+import App from './src';
 
 AppRegistry.registerComponent(appName, () => App);
--- level-1/aula3-react-native/src/index.js
+++ level-1/aula3-react-native/src/index.js
@@ -1,8 +1,86 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useEffect, useState, Fragment } from 'react';
+import api from './services/api'
 
-export default () => (
-  <View>
-    <Text>Hwllo World!</Text>
-  </View>
-);
+import {
+  SafeAreaView,
+  FlatList, Text,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity
+} from 'react-native';
+
+export default () => {
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('projects');
+      setProjects(response.data);
+    })()
+
+  }, [])
+
+  const handleSubmitProject = async () => {
+    const data = {
+      title: "CaralhoooooXXXXXXXXX",
+      owner: "Mateus Pinto GarciXXXXXXXXXXXXXXa"
+    }
+    const response = await api.post('projects', data);
+    setProjects([...projects, response.data])
+  }
+
+  return (
+    <Fragment>
+      <SafeAreaView style={styles.container}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="#74d"
+        />
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item }) => (
+            <Text style={styles.text}>  {item.title} </Text>
+          )}
+        />
+        <TouchableOpacity
+          onPress={handleSubmitProject}
+          activeOpacity={0.6}
+          style={styles.button}>
+          <Text style={styles.buttontext}>
+            Adicionar
+          </Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+
+    </Fragment >
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#74d'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+
+  button: {
+    backgroundColor: '#FFF',
+    margin: 20,
+    padding: 15,
+    borderRadius: 5
+  },
+
+  buttontext: {
+    textAlign: 'center',
+    color: '#000',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+
+})
