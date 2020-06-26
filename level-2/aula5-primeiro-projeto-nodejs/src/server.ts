86f73e10c1d3e760b4b29215608cef84c957b975
mpgxc <mpgx5.c@gmail.com>
2020-06-26 06:44:36 -0300

    feat(aula5) add eslint, editorconfig andprettier for styleguide code

--- level-2/aula5-primeiro-projeto-nodejs/src/server.ts
+++ level-2/aula5-primeiro-projeto-nodejs/src/server.ts
@@ -1,14 +1,14 @@
+/* eslint-disable no-console */
 import express from 'express';
 
 const server = express();
 server.use(express.json());
 
-
 server.post('/', (request, response) => {
-  const { name, email } = request.body
-  return response.json({ message: name, email });
-})
+    const { name, email } = request.body;
+    return response.json({ message: name, email });
+});
 
 server.listen(3333, () => {
-  console.log('Server GoStck! Iniciado!')
-})
+    console.log('Server GoStck! Iniciado!');
+});
